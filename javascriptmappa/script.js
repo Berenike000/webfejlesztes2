8fc35598c5102129c7665073e1f6cd5705c8163d
Berenike000 <berenike.bagi@stud.uni-corvinus.hu>
2024-12-02 13:14:01 +0100

    fájlok feltöltése

---
+++ javascriptmappa/script.js
@@ -0,0 +1,20 @@
+const sorozate = document.getElementById("sorozattagja").checked
+document.write(sorozate)
+const kortarse = document.getElementById("kortars").checked
+const humorose = Number(document.getElementById("humorose").value)
+const urutazas = document.getElementById("urutaz").checked
+const tarskritika = document.getElementById("tarskrit").checked
+const ufo = document.getElementById("ufo").checked
+const ai = document.getElementById("ai").checked
+const disztopia = document.getElementById("disz").checked
+const posztapok = document.getElementById("poszta").checked
+const uropera = document.getElementById("urop").checked
+const krimi = document.getElementById("krimi").checked
+
+
+
+document.getElementById("ajanlogomb").addEventListener("click", konyvajanlas)
+
+function konyvajanlas(){
+
+}
